0e31e915529a6451ca4414199cf57123057775c9
kaustubh <kaustubhkagrawal@gmail.com>
2024-01-29 15:27:35 +0530

    adding cors

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,7 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
+import { setCors } from "../utils/cors.utils";
 
 export default (req: VercelRequest, res: VercelResponse) => {
+  setCors(res);
   return res.json({ message: "Hello World" });
 };
---
+++ utils/cors.utils.ts
@@ -0,0 +1,14 @@
+import { VercelResponse } from "@vercel/node";
+
+export const setCors = (res: VercelResponse) => {
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,POST,PATCH,DELETE,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+};
